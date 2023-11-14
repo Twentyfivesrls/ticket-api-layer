8572e2d97c198996b7ca03ed54b672acc8db9d70
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-11-14 16:25:55 +0100

    change ip in application properties

--- src/main/java/com/twentyfive/ticketapilayer/TicketApiLayerApplication.java
+++ src/main/java/com/twentyfive/ticketapilayer/TicketApiLayerApplication.java
@@ -2,11 +2,12 @@ package com.twentyfive.ticketapilayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.twentyfive.authorizationflow", "com.twentyfive.ticketapilayer"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class TicketApiLayerApplication {
 
