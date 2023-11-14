6ca398c4c947a1d51aadaa6ef4e42e1faa4ce0c5
twentyfive <twentyfivesrls@gmail.com>
2023-11-14 12:52:02 +0100

    fixed authorization-flow

--- src/main/java/com/twentyfive/ticketapilayer/controllers/TicketController.java
+++ src/main/java/com/twentyfive/ticketapilayer/controllers/TicketController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,6 +38,7 @@ public class TicketController {
     }
 
     @PostMapping("/list")
+    @PreAuthorize("hasRole('ROLE_single_realm_role')")
     public ResponseEntity<Object> getTicketList(@RequestBody TicketFilter ticket,
                                                 @RequestParam(defaultValue = "0") int page,
                                                 @RequestParam(defaultValue = "5") int size) {
